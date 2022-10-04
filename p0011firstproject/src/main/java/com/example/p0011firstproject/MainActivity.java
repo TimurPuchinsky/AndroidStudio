c044e8f2689c2944e6ce6d98406600670a1607f5
Timur Puchinsky <timur.2.puchinsky@gmail.com>
2022-10-04 14:40:06 +0300

    лаб 3. Добавил логику к кнопкам операций, равенства и очистки

--- p0011firstproject/src/main/java/com/example/p0011firstproject/MainActivity.java
+++ p0011firstproject/src/main/java/com/example/p0011firstproject/MainActivity.java
@@ -8,6 +8,10 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    String oldNumber;
+    String operator;
+    Boolean isNew = true;
+
     EditText editText;
 
     @Override
@@ -19,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickNumber(View view) {
+        if(isNew)
+            editText.setText("");
+        isNew = false;
         String number = editText.getText().toString();
         switch (view.getId()) {
             case R.id.but0: number = number + "0"; break;
@@ -31,7 +38,39 @@ public class MainActivity extends AppCompatActivity {
             case R.id.but7: number = number + "7"; break;
             case R.id.but8: number = number + "8"; break;
             case R.id.but9: number = number + "9"; break;
+            case R.id.butDot: number = number + "."; break;
+            case R.id.butPlusMinus: number = number + "-"; break;
         }
         editText.setText(number);
+    }
+
+    public void operation(View view) {
+        isNew = true;
+        oldNumber = editText.getText().toString();
+        switch(view.getId()){
+            case R.id.butMinus: operator="-"; break;
+            case R.id.butPlus: operator="+"; break;
+            case R.id.butDivide: operator="/"; break;
+            case R.id.butMultiply: operator="*"; break;
+            case R.id.butPercent: operator="%"; break;
+        }
+    }
+
+    public void clickEqual(View view) {
+        String newNumber = editText.getText().toString();
+        Double result = 0.0;
+        switch(operator){
+            case "-": result = Double.parseDouble(oldNumber) - Double.parseDouble(newNumber); break;
+            case "+": result = Double.parseDouble(oldNumber) + Double.parseDouble(newNumber); break;
+            case "*": result = Double.parseDouble(oldNumber) * Double.parseDouble(newNumber); break;
+            case "/": result = Double.parseDouble(oldNumber) / Double.parseDouble(newNumber); break;
+            case "%": result = Double.parseDouble(oldNumber) % Double.parseDouble(newNumber); break;
+        }
+        editText.setText(result+"");
+    }
+
+    public void clickClear(View view) {
+        editText.setText("0");
+        isNew = true;
     }
 }
